2956a9ba20f4b18bf2fbaf23022cd986be085296
Yaowang-bjtu <wangyao@bjtu.edu.cn>
2019-04-30 08:26:15 +0800

    add AnomalyDetection class
    as an interface for real detection task

---
+++ detection.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+from options import Options
+from lib.data import load_data
+from lib.model import Ganomaly
+
+class AnomalyDetection():
+    
+    def __init__(self,model,options={}):
+        self.model = model
+
+    def detect(image):
+        result = []
+        return result
